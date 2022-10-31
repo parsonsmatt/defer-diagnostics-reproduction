5eeccdaddbddce7e27ac386577a20e9f8da9d5b1
parsonsmatt <parsonsmatt@gmail.com>
2022-10-31 14:43:45 -0600

    ok

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,8 @@ module Main (main) where
 
 import Lib
 
+
+ohno = 2
+
 main :: IO ()
 main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+import Lib.A
+import Lib.B
+
+blah = 3
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
